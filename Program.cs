ec3027b15d4ff6f90e2bb5ea2127be10f325d9f6
gitea <admin@admin.com>
2024-07-04 09:47:06 +0000

    first commit

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+public class Base
+{
+    public virtual void Print()
+    {
+        Console.WriteLine("Base Class Print");
+    }
+}
+
+public class Derived : Base
+{
+    public override void Print()
+    {
+        Console.WriteLine("Derived Class Print");
+    }
+}
+
+public class Program
+{
+    public static void Main ()
+    {
+        Base B = new Derived();
+        B.Print();
+    }
+}
+
